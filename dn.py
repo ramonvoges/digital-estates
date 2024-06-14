85331bbe07270b611f5051009d23e1277483989a
Ramon Voges <r.voges@dnb.de>
2024-06-14 16:52:51 +0200

    XML Parser für Datetyp ergänzen

--- dn.py
+++ dn.py
@@ -2,6 +2,8 @@ import os
 import pandas as pd
 from datetime import datetime
 import sys
+import requests
+from xml.etree import ElementTree as ET
 
 def get_file_info(directory_path):
     file_info_list = []
@@ -10,6 +12,7 @@ def get_file_info(directory_path):
             file_path = os.path.join(root, file)
             file_stats = os.stat(file_path)
             file_extension = os.path.splitext(file)[1]
+            file_type = get_file_type(file_path)
             file_info = {
                 "Laufende Nummer": i,
                 "Dateiname": file,
@@ -18,10 +21,24 @@ def get_file_info(directory_path):
                 "Änderungsdatum": datetime.fromtimestamp(file_stats.st_mtime).strftime('%Y-%m-%d %H:%M:%S'),
                 "Erstellungsdatum": datetime.fromtimestamp(file_stats.st_ctime).strftime('%Y-%m-%d %H:%M:%S'),
                 "Dateigröße (Bytes)": file_stats.st_size,
+                "Dateityp": file_type,
                 "Bemerkungen": ""
             }
             file_info_list.append(file_info)
     return file_info_list
+
+def get_file_type(file_path):
+    url = "http://etc.dnb.de.appr/fits/examine"
+    with open(file_path, 'rb') as file:
+        response = requests.post(url, files={'datafile': file}, verify=False)
+    if response.status_code == 200:
+        xml_root = ET.fromstring(response.content)
+        print(xml_root)
+        format_element = xml_root.find('.//identity[@format]')
+        print(format_element)
+        if format_element is not None:
+            return format_element.get('format')
+    return "Unbekannt"
 
 def create_excel(file_info_list, output_file):
     df = pd.DataFrame(file_info_list)
