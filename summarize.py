6e5c60620860e5962ee37dccd1937d4b8ae1223a
Ramon Voges <r.voges@dnb.de>
2024-06-17 17:18:34 +0200

    Summarize prüft Dateiendung

--- summarize.py
+++ summarize.py
@@ -1,6 +1,54 @@
 import sys
+import os
 
+import docx2txt
+import PyPDF2
 import ollama
+
+def read_file_content(file_path):
+    # Überprüfen, ob die Datei existiert
+    if not os.path.isfile(file_path):
+        raise FileNotFoundError(f"Die Datei {file_path} wurde nicht gefunden.")
+    
+    # Dateiendung extrahieren
+    file_extension = os.path.splitext(file_path)[1].lower()
+    
+    # Inhalt der Datei basierend auf der Dateiendung auslesen
+    if file_extension == '.docx':
+        # Word-Dokument
+        content = docx2txt.process(file_path)
+    elif file_extension == '.pdf':
+        # PDF-Dokument
+        content = ""
+        with open(file_path, 'rb') as file:
+            reader = PyPDF2.PdfFileReader(file)
+            for page_num in range(reader.numPages):
+                page = reader.getPage(page_num)
+                content += page.extract_text()
+    elif file_extension == '.txt' or file_extension == '.md':
+        # Reine Text-Datei
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+    else:
+        raise ValueError(f"Die Dateiendung {file_extension} wird nicht unterstützt.")
+    
+    return content
+
+
+def summarize_content(content):
+    prompt = "Deine Aufgabe ist es, den gegebenen Text in ungefähr 300 Wörtern zusammenzufassen. Gebe nur die Zusammenfassung wieder ohne weitere Angaben."
+    response = ollama.chat(
+        model="llama3",
+        messages=[
+            {
+                'role': 'user',
+                'content': f"{prompt}. Das ist der Text: {content}"
+            },
+        ],
+        )
+    summary = response['message']['content']
+    return summary
+
 
 def summarize_file(file_path):
     # Fasse den Inhalt der übergebenen Datei zusammen
@@ -26,5 +74,6 @@ if __name__ == "__main__":
         sys.exit(1)
     
     file_path = sys.argv[1]
-    summary = summarize_file(file_path)
-    print("Summary:", summary)
+    content = read_file_content(file_path=file_path)
+    summary = summarize_content(content)
+    print(summary)
